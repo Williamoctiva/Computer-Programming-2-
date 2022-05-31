efe7394a99e619ac84d679e99f971e1343667bb8
Williamoctiva <101566364+Williamoctiva@users.noreply.github.com>
2022-05-31 11:15:27 +0800

    Update Activity 3.0.c

--- Activity 3.0.c
+++ Activity 3.0.c
@@ -9,16 +9,16 @@ int main()
     
     if(n<=10){
        if(n>=5){
-       for(i=0; i<n; ++i){
-        printf("Enter Elements %d:",i+1);
-        scanf("%d",&marks[i]);
-        sum+=marks[i];
+          for(i=0; i<n; ++i){
+            printf("Enter Elements %d:",i+1);
+            scanf("%d",&marks[i]);
+            sum+=marks[i];
+          }
        }
-    }
     Total= sum;
     printf("Sum of all array elements = %d", Total);
-    
-    return 0;
     }
-
+    return 0;
  }
+
+ 
